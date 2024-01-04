c41e9e59ab6d2c3a84c49a231e3f30967c07eb4d
abror <inagamovzaynobiddin@gamil.com>
2024-01-04 22:15:20 +0500

    ...

---
+++ static/project/Plugins/Tailwind/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+  theme: {
+    extend: {
+
+    },
+    screens:{
+      xs: '450px',
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1280px',
+    },
+    container: {
+      padding: {
+        DEFAULT: '0.8rem',
+        sm: '0.9rem',
+        lg: '1rem',
+        xl: '1.2rem'
+      },
+      center: 'true',
+    },
+  },
+};
